3863bcf7dc541e4e2d626a48a5b74c4bc29093ef
DAW <juanmiguel.valle@iesdonana.org>
2025-06-15 18:12:36 +0200

    migraciones

---
+++ app/Models/Ordenador.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Ordenador extends Model
+{
+    /** @use HasFactory<\Database\Factories\OrdenadorFactory> */
+    use HasFactory;
+}
